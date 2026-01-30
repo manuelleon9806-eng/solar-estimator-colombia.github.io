e7805d6c4237828437041c16b22931e9eb730450
manuelleon9806-eng <manuelleon9806@gmail.com>
2026-01-30 12:55:16 -0500

    Add showStep function to manage active steps

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const steps = document.querySelectorAll(".step");
 let currentStep = 0;
+function showStep(stepIndex) {
+    steps.forEach(step => step.classList.remove("active"));
+    steps[stepIndex].classList.add("active");
+}
+
 
