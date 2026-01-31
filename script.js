068c2c486c3a802fb298a9d9fea5068a0fb5b3ef
manuelleon9806-eng <manuelleon9806@gmail.com>
2026-01-31 18:42:01 -0500

    Enhance progress bar and update result display
    Update progress bar width and modify result message.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let consumo = 0;
+const progressBar = document.getElementById("progress-bar");
 
 document.getElementById("btn-step-1").addEventListener("click", () => {
   const input = document.getElementById("consumo").value;
@@ -12,18 +13,20 @@ document.getElementById("btn-step-1").addEventListener("click", () => {
 
   document.getElementById("step-1").classList.remove("active");
   document.getElementById("step-2").classList.add("active");
+
+  progressBar.style.width = "66%";
 });
 
 document.getElementById("btn-step-2").addEventListener("click", () => {
   document.getElementById("step-2").classList.remove("active");
   document.getElementById("step-3").classList.add("active");
-  
-document.getElementById("resultado").innerText =
-  `Con un consumo aproximado de ${consumo} kWh mensuales,
-  la energía solar podría ser una buena opción para ti en Colombia.
 
-  Muchas personas con consumos similares logran reducir
-  su factura y ganar mayor control sobre sus gastos de energía.`;
+  progressBar.style.width = "100%";
+
+  document.getElementById("resultado").innerText =
+    `Con un consumo aproximado de ${consumo} kWh mensuales,
+    la energía solar podría ser una buena opción para ti en Colombia.`;
+});
 
 
 
